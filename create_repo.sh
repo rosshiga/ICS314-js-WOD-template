43a91ef1acce1d5b00aad861c31560b91f1d2cfc
Austin Haigh <haigh@hawaii.edu>
2018-09-22 16:52:56 -1000

    added script

---
+++ create_repo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source .create_repo.env
+
+if [[ $# -ne 1 ]]
+then
+	echo Usage: $0 [repo name]
+	exit 1
+fi
+
+
+RESPONSE=$(curl -sS -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"'$1'", "private":"true", "auto_init":"true"}')
+if [[ $(grep "Bad credentials" <<< "$RESPONSE") ]]
+then
+	echo "Invalid password"
+	exit 1
+fi
+if [[ $(grep "errors" <<< "$RESPONSE") ]]
+then 
+	grep message <<< "$RESPONSE" | sed -r 's/.*message": "([a-zA-Z \.]+).*/\1/'
+	exit 1
+fi
+
+curl -sS -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/repos/$USERNAME/$1/collaborators/$GRADER -d '{"permission":"pull"}' -X PUT
+
+shopt -s dotglob
+
+cp -rap js_template/. $1 &&
+rename js_template $1 $1/* &&
+find $1 -type f -exec sed -r -i "s/js_template/$1/" {} + &&
+git clone git@github.com:$USERNAME/$1.git tmp &&
+mv tmp/* $1 &&
+rm -rf tmp &&
+
+cd $1 
+idea $1.js 2> /dev/null
+git add index.html $1.js &&
+git commit -m "WOD done" &&
+git push
